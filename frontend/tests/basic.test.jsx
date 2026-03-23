73859ce35e7e2c928caa8288e069bc132b757f61
chandrasekharreddyk <chandrasekharreddy3897@gmail.com>
2026-03-24 01:53:56 +0530

    changed test scripts to test failed cases

--- frontend/tests/basic.test.jsx
+++ frontend/tests/basic.test.jsx
@@ -3,6 +3,6 @@ import { describe, it, expect } from "vitest";
 describe("basic frontend test", () => {
   it("title contains MERN", () => {
     const title = "MERN E-Commerce Store";
-    expect(title).toContain("MERN");
+    expect(title).toContain("Chandu");
   });
 });
