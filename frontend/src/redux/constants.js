1b6b5b67a8079c795a57015f88d70f38849e12f5
chandrasekharreddyk <chandrasekharreddy3897@gmail.com>
2026-03-27 02:45:04 +0530

    Added and updated helm files - working fine

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
+export const BASE_URL = "/api";
 
 export const USERS_URL = `${BASE_URL}/users`;
 export const CATEGORY_URL = `${BASE_URL}/category`;
